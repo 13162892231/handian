9f7e6af5d4e766502278e866e386240a349ec779
13162892231 <13162892231>
2020-03-31 15:44:46 +0800

    说明信息

---
+++ Aessts/js/Comm.js
@@ -0,0 +1,270 @@
+﻿var audioPaused = true;
+var bgindex = 0;
+var media = document.getElementById("music");
+var myVideo = document.getElementById("video1");
+
+
+
+$(function($) {
+    $('.loader').remove();
+    var mySwiper = new Swiper('.swiper-container', {
+        mode: 'horizontal',
+        onSwiperCreated: function(swiper) {
+            $(".zhou_one").addClass("zhou_onemove");
+            $(".left").addClass("zhou_onemove");
+            $(".right").addClass("zhou_twomove");
+            $(".zhou_two").addClass("zhou_twomove");
+
+            setTimeout(function() {
+                $(".lunshou").addClass("lunshoumove");
+                $(".logo").addClass("slidetop");
+                $(".centent").addClass("Display");
+                $(".logobak").addClass("Display");
+                $(".picjuan").fadeOut();
+            }, 2500)
+
+        },
+        onSlideChangeStart: function(swiper) {
+            clearclass();
+            activeLoopIndex = swiper.activeIndex;
+            switch (swiper.activeIndex) {
+                case 0:
+                    $(".bg_img img").css('transition', '0.5s').css('transform', 'translateX(0)');
+                    $(".bg_zhu img").css('transition', '0.5s').css('transform', 'translateX(0)');
+                    break;
+                case 1:
+                    $(".bg_img img").css('transition', '0.5s').css('transform', 'translateX(-200px)');
+                    $(".bg_zhu img").css('transition', '0.5s').css('transform', 'translateX(-20px)');
+                    break;
+                case 2:
+                    $(".bg_img img").css('transition', '0.5s').css('transform', 'translateX(-600px)');
+                    $(".bg_zhu img").css('transition', '0.5s').css('transform', 'translateX(-40px)');
+                    break;
+                case 3:
+                    $(".bg_img img").css('transition', '0.5s').css('transform', 'translateX(-800px)');
+                    $(".bg_zhu img").css('transition', '0.5s').css('transform', 'translateX(-120px)');
+                    break;
+                case 4:
+                    $(".bg_img img").css('transition', '0.5s').css('transform', 'translateX(-1000px)');
+                    $(".bg_zhu img").css('transition', '0.5s').css('transform', 'translateX(-500px)');
+                    break;
+            }
+        },
+        onSlideChangeEnd: function(swiper) {
+
+            activeLoopIndex = swiper.activeIndex;
+            switch (swiper.activeIndex) {
+                case 0:
+                    $(".lunshou").addClass("lunshoumove");
+                    $(".logo").addClass("slidetop");
+                    $(".centent").addClass("Display");
+                    $(".logobak").addClass("Display");
+                    $(".vidiompg").fadeOut();
+                    if (myVideo.paused) {
+
+                    } else {
+                        myVideo.pause();
+                        media.play();
+                    }
+                    break;
+                case 1:
+                    $(".lunshou_two").addClass("lunshoumove");
+                    $(".three_word").addClass("slidetop");
+                    $(".three_word_one").addClass("slidetop_one");
+                    $(".vidiofang_two").addClass("vidiofang_twomove");
+                    $(".vidiofang").fadeIn();
+                    break;
+                case 3:
+                    $(".vidiompg").fadeOut();
+                    $(".lunshou_three").addClass("lunshoumove");
+                    if (myVideo.paused) {} else {
+                        myVideo.pause();
+                        media.play();
+                    }
+                    $(".buttoncss").addClass("Display_date");
+                    $(".HotDate").addClass("Display_date");
+                    $(".tou").addClass("slidedown");
+                    $(".fourword").addClass("Display");
+                    $(".fourword_two").addClass("Display");
+                    break;
+                case 4:
+                    $(".fiveword").addClass("Display2");
+                    $(".gaunzhu").addClass("gaunzhuMove");
+                    $(".share").addClass("shareMove");
+                    break;
+            }
+        }
+    });
+
+    var jmSwiper = new Swiper('.jmcontainer',{
+        slidesPerView:'auto',
+        mode:'vertical',
+        watchActiveIndex: true
+    });
+});
+
+
+
+$(".vidiofang").click(function() {
+    $(".vidiompg").fadeIn();
+    myVideo.play();
+    media.pause();
+});
+$(".share").click(function() {
+    $(".jiantou").fadeIn();
+
+});
+$(".gaunzhu").click(function() {
+    $("#guanz").fadeIn();
+
+});
+$("#guanz").click(function() {
+    $("#guanz").fadeOut();
+});
+$(".jiantou").click(function() {
+    $(".jiantou").fadeOut();
+});
+$(".exit").click(function() {
+    $(".vidiompg").fadeOut();
+    myVideo.pause();
+    media.play();
+});
+       $(".gongzhong").click(function(){
+       $(".gongzhong").fadeOut();
+
+       });
+
+$(".globalAudio").bind("click", function() {
+
+    if (media.paused) {
+        media.play();
+        audioPaused = true;
+        $(this).addClass("play");
+    } else {
+        media.pause();
+        audioPaused = false;
+        $(this).removeClass("play");
+    }
+});
+
+function clearclass() {
+    $(".logo").removeClass("slidetop");
+    $(".centent").removeClass("Display");
+    $(".logobak").removeClass("Display");
+    $(".lunshou").removeClass("lunshoumove");
+    $(".lunshou_two").removeClass("lunshoumove");
+    $(".lunshou_three").removeClass("lunshoumove");
+    $(".vidiofang_two").removeClass("vidiofang_twomove");
+    $(".vidiofang").fadeOut();
+    $(".three_word").removeClass("slidetop");
+    $(".three_word_one").removeClass("slidetop_one");
+    $(".vidiofang").removeClass("slidetop");
+    $(".tou").removeClass("slidedown");
+    $(".fourword").removeClass("Display");
+    $(".fourword_two").removeClass("Display");
+    $(".fiveword").removeClass("Display2");
+    $(".gaunzhu").removeClass("gaunzhuMove");
+    $(".share").removeClass("shareMove");
+    $(".buttoncss").removeClass("Display_date");
+    $(".gongzhong").fadeOut();
+    $("#guanz").fadeOut();
+    $(".jiantou").fadeOut();
+    $(".HotDate").removeClass("Display_date");
+}
+
+function guanzhu() {
+    var Time = new Date();
+    var EnTime = new Date('2015/01/19 00:00:00');
+    if (Time >= EnTime) {
+        window.location = "Vote.html";
+    }
+    else{
+        alert("对不起，投票已结束!!!");
+    }
+
+}
+
+
+
+document.addEventListener('WeixinJSBridgeReady', function() {
+    var _WeixinJSBridge = {};
+    Object.keys(WeixinJSBridge).forEach(function(key) {
+        _WeixinJSBridge[key] = WeixinJSBridge[key];
+    });
+    Object.keys(WeixinJSBridge).forEach(function(key) {
+        if (typeof WeixinJSBridge[key] === 'function') {
+            WeixinJSBridge[key] = function() {
+                try {
+                    var args = arguments.length > 0 ? arguments[0] : {},
+                        runOn3rd_apis = args.__params ? args.__params.__runOn3rd_apis || [] : [];
+                    ['menu:share:timeline', 'menu:share:appmessage', 'menu:share:qq'].forEach(function(menu) {
+                        runOn3rd_apis.indexOf(menu) === -1 && runOn3rd_apis.push(menu);
+                    });
+                } catch (e) {}
+                return _WeixinJSBridge[key].apply(WeixinJSBridge, arguments);
+            };
+        }
+    });
+
+    var shareData = {
+        "appid": "",
+        "title": "翰典之夜",
+        "link": "http://60.191.57.21:8019/hd/mainPage.html",
+        "img_url": "http://60.191.57.21:8019/hd/Aessts/Images/logo.jpg",
+        "desc": "2015年大型新春晚会———博融天下·思享未来",
+        "img_width": "200",
+        "img_height": "200"
+    };
+
+    // 分享到朋友圈
+    WeixinJSBridge.on('menu:share:timeline', function() {
+
+        WeixinJSBridge.invoke('shareTimeline', shareData, function(res) {
+            // 返回res.err_msg,取值
+            // share_timeline:cancel 用户取消
+            // share_timeline:fail　发送失败
+            // share_timeline:confirm 发送成功
+            WeixinJSBridge.log(res.err_msg);
+
+            if (res.err_msg == 'share_timeline:confirm') {
+                _czc.push(["_trackPageview", "/hdzy/zhuanfa", "referer_url"]);
+            };
+        });
+
+    });
+
+    //发送给朋友
+    WeixinJSBridge.on('menu:share:appmessage', function() {
+        WeixinJSBridge.invoke('sendAppMessage', shareData, function(res) {
+            // 返回res.err_msg,取值
+            // share_timeline:cancel 用户取消
+            // share_timeline:fail　发送失败
+            // share_timeline:confirm 发送成功
+            WeixinJSBridge.log(res.err_msg);
+
+            if (res.err_msg == 'share_timeline:confirm') {
+                _czc.push(["_trackPageview", "/hdzy/zhuanfa", "referer_url"]);
+            };
+        });
+
+    });
+
+    //发送给朋友
+    WeixinJSBridge.on('menu:share:qq', function() {
+        WeixinJSBridge.invoke('shareQQ', shareData);
+
+    });
+
+    // 分享 for ios
+    WeixinJSBridge.on('general:share', function(argv) {
+        if (argv) {
+            if (argv.shareTo === 'timeline') {
+                WeixinJSBridge.invoke('shareTimeline', shareData);
+            } else if (argv.shareTo === 'friend') {
+                WeixinJSBridge.invoke('sendAppMessage', shareData);
+            } else if (argv.shareTo === 'QQ') {
+                WeixinJSBridge.invoke('shareQQ', shareData);
+            }
+        }
+    });
+});
---
+++ Aessts/js/voteJs.js
@@ -0,0 +1,210 @@
+﻿var mySwiper_zero = null;
+
+var slider_content = null;
+
+
+var mySwiper = new Swiper('.myswiper', {
+    mode: 'horizontal',
+    onSwiperCreated: function (swiper) {
+        addswiper(0);
+        $(".icon2").addClass("iconAnimation2");
+        $(".icon3").addClass("iconAnimation3");
+
+    },
+    onSlideChangeStart: function (swiper) {
+
+
+        switch (swiper.activeIndex) {
+            case 0:
+                addswiper(swiper.activeIndex);
+                break;
+            case 1:
+                addswiper(swiper.activeIndex);
+                break;
+            case 2:
+                addswiper(swiper.activeIndex);
+                break;
+
+            case 3:
+                addswiper(swiper.activeIndex);
+                break;
+            case 4:
+                addswiper(swiper.activeIndex);
+                break;
+            case 5:
+                addswiper(swiper.activeIndex);
+                break;
+            case 6:
+                addswiper(swiper.activeIndex);
+                break;
+
+            case 7:
+                addswiper(swiper.activeIndex);
+                break;
+            case 8:
+                addswiper(swiper.activeIndex);
+                break;
+            case 9:
+                addswiper(swiper.activeIndex);
+                break;
+            case 10:
+                addswiper(swiper.activeIndex);
+                break;
+
+
+        }
+    }
+});
+
+
+function addswiper(index) {
+    mySwiper_zero = new Swiper('.swiperVertical_'+index, {
+        slidesPerView: 'auto',
+        mode: 'vertical',
+        
+        watchActiveIndex: true,
+    });
+
+    var Program = {ProgramID:index+1}
+    $.ajax({
+        url: "/_Interface/HD2/PostWhereC",
+        type: "post",
+        data: Program,
+
+        success: function (data) {
+            if (data == null) {
+
+            } else {
+                mySwiper_zero.removeAllSlides();
+                var content = null;
+                for (var i = 0; i < data.length; i++) {
+
+                    if (data[i].Content.length > 30) {
+                        content = data[i].Content.substr(0, 30)+"...";
+                    }
+                    else { content = data[i].Content;}
+                    slider_content = "<div class='swiper-slide'>"+
+                    "<p class='pname'>" + data[i].NickName + "</p>" +
+                    "<p class='previewcontent'>" + content + "</p>" +
+                "</div>";
+                    mySwiper_zero.prependSlide(slider_content);
+                }
+
+            }
+           
+        }
+    });
+
+}
+
+$("#fabiao").click(function () {
+    var nickname = $("#nickname").val();
+    var reviewcont = $("#reviewcont").val();
+    if (nickname == null || reviewcont == null) {
+        return false;
+    } else {
+        var Comment = { NickName: $("#nickname").val(), Content: $("#reviewcont").val(), ProgramID: mySwiper.activeIndex + 1 };
+        $.ajax({
+            url: "/_Interface/HD2/PostComment",
+            type: "post",
+            data: Comment,
+
+            success: function (data) {
+                var subcontent = null;
+                if (data.State == "1") {
+                    alert("发表成功！！");
+                    if (reviewcont.length > 30) {
+                        subcontent = reviewcont.substr(0, 30)+"...";
+                    }
+                    else { subcontent = reviewcont; }
+                    slider_content = "<div class='swiper-slide'>" +
+                    "<p class='pname'>" + nickname + "</p>" +
+                    "<p class='previewcontent'>" + subcontent + "</p>" +
+                "</div>";
+                    mySwiper_zero.prependSlide(slider_content);
+                    $(".mask").fadeOut();
+                    $(".divreview").fadeOut();
+                } else {
+                    alert("发表失败！！")
+                }
+            }
+        });
+    }
+   
+
+});
+$("#tou").click(function () {
+    var name = $("#name").val();
+    var banji = $("#banji").val();
+    var phone = $("#phone").val();
+    if (getCookie("value") != null) {
+        alert("不好意思，您已经投过票了！！")
+    }
+    else {
+        if (name =="" || banji == "" || phone =="") {
+            alert("请填写完整的信息！！")
+            return false;
+        }
+        else {
+            var myreg = /^(((1[3-8][0-9]{1})|159|153)+\d{8})$/;
+
+            if (!myreg.test(phone)) {
+                alert("请输入有效的电话号码！")
+            }
+            else {
+                var PostVote = { Name: name, Class: banji, Phone: phone, ProgramID: mySwiper.activeIndex + 1 };
+                $.ajax({
+                    url: "/_Interface/HD2/PostVote",
+                    type: "post",
+                    data: PostVote,
+
+                    success: function (data) {
+                        if (data.State == "1") {
+                            alert("投票成功！！！");
+                            setCookie("value", "521");
+                            $(".mask").fadeOut();
+                            $(".divvote").fadeOut();
+                        }
+                        else {
+                            alert("投票失败！！！");
+                        }
+                    }
+                });
+            }
+
+        }
+    }
+   
+});
+
+
+$(".reviewbtu").click(function () {
+    $(".mask").show();
+    $(".divreview").show();
+});
+$(".votebtu").click(function () {
+    $(".mask").show();
+    $(".divvote").show();
+});
+
+
+$(".exit").click(function () {
+    $(".mask").hide();
+    $(".divvote").hide();
+    $(".divreview").hide();
+});
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+
+    if (arr = document.cookie.match(reg))
+
+        return unescape(arr[2]);
+    else
+        return null;
+}
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
